55c75f3d9292faad329ce62412808cf586a72dfa
kaitoulee <hackerheilian@vip.qq.com>
2016-04-22 20:16:32 +0800

    翻页效果
    第一版

---
+++ 翻页效果/imgeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  imgeViewController.swift
+//  翻页效果
+//
+//  Created by kaitouLee on 16/4/22.
+//  Copyright © 2016年 kaitou Lee. All rights reserved.
+//
+
+import UIKit
+
+class imgeViewController: UIViewController {
+
+    @IBAction func backButClicked(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
